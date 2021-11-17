20c9094b351d4052465c72dbf45e84bac4eb5a6a
kevinGT99 <gironkevin130@gmail.com>
2021-11-16 22:11:11 -0300

    formularios

--- app.js
+++ app.js
@@ -4,9 +4,22 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req,res)=> {
-    res.sendFile(path.resolve(__dirname + '/views/home.html'))
-})
 app.listen(3000,() => {
     console.log('servidor corriendo en el puerto 3000')
 })
+app.get('/', (req,res)=> {
+    res.sendFile(path.resolve(__dirname + '/views/home.html'))
+})
+
+app.get('/register', (req,res)=> {
+    res.sendFile(path.resolve(__dirname + '/views/register.html'))
+})
+
+app.get('/login', (req,res)=> {
+    res.sendFile(path.resolve(__dirname + '/views/login.html'))
+})
+
+app.post('/', (req,res)=> {
+    res.sendFile(path.resolve(__dirname + '/views/home.html'))
+})
+
