0cc283926f188aafbeaf602b708221e489cc2dea
kevinGT99 <gironkevin130@gmail.com>
2021-11-22 23:13:15 -0300

    agregando algunos estilos

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000,() => {
-    console.log('servidor corriendo en el puerto 3000')
-})
+app.listen(process.env.PORT || 4000, ()=>{
+    console.log('Servidor funcionando');
+});
+
 app.get('/', (req,res)=> {
     res.sendFile(path.resolve(__dirname + '/views/home.html'))
 })
